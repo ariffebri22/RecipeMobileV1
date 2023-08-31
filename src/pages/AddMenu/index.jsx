3562ed1e83d5916d0e33b05346c621aed376176e
ariffebri22 <muhammadariffebriansyah@gmail.com>
2023-08-31 09:16:26 +0700

    Slicing AddMenu (Done)

--- src/pages/AddMenu/index.jsx
+++ src/pages/AddMenu/index.jsx
@@ -1,14 +1,58 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View, TextInput} from 'react-native';
 import React from 'react';
+import {Input, Icon, Text} from '@rneui/themed';
+import {ButtonCta} from '../../components';
 
 const AddMenu = () => {
   return (
-    <View>
-      <Text>AddMenu</Text>
+    <View style={styles.container}>
+      <Text
+        style={{
+          fontSize: 30,
+          color: '#EFC81A',
+          fontWeight: '700',
+          marginTop: 80,
+          marginBottom: 30,
+        }}>
+        Add Your Recipe
+      </Text>
+      <Input
+        inputContainerStyle={styles.input}
+        placeholder="Title"
+        leftIcon={
+          <Icon
+            marginLeft={10}
+            type="feather"
+            name="book-open"
+            size={28}
+            color="#8B8A8F"
+          />
+        }
+      />
+      <Input
+        inputContainerStyle={styles.input}
+        placeholder="Ingredients"
+        numberOfLines={6}
+      />
+      <Input inputContainerStyle={styles.input} placeholder="Add Photo" />
+      <Input inputContainerStyle={styles.input} placeholder="Category" />
+      <ButtonCta title="POST" />
     </View>
   );
 };
 
 export default AddMenu;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    padding: 0,
+    fontSize: 16,
+    backgroundColor: '#F5F5F5',
+    borderRadius: 10,
+    marginHorizontal: 20,
+  },
+});
